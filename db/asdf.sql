ff70d4f013eb7de5fe31a9c7899af55f66f5a215
Dan Devarajan <ddevarajan22@gmail.com>
2020-01-22 22:18:36 -0600

    Made some dope changes

---
+++ db/asdf.sql
@@ -0,0 +1,16 @@
+
+USE xdq3amktibtlrhrl;
+
+
+CREATE TABLE user (
+	id VARCHAR(15),
+    nickname VARCHAR(20),
+    email VARCHAR(20)
+
+
+);
+
+INSERT INTO user(id, nickname, email) VALUES ("asdfmane", "asdf jkl", "somehaters@gmail.com");
+-- INSERT INTO user(user_id, nickname, email) VALUES ("Portabelo Mushroom/Onion", false);
+-- INSERT INTO user(user_id, nickname, email) VALUES ("Bacon Wrapped Wagyu Beef/Aged Gouda", false);
+
